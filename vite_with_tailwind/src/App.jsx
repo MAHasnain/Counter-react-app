05f64cf50c991a70f91a2568a374a1804eeffe5a
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-11-14 17:31:28 +0500

    feat: added number increase and decrease feature

--- vite_with_tailwind/src/App.jsx
+++ vite_with_tailwind/src/App.jsx
@@ -1,38 +1,33 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  const decreaseNum = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      return
+    }
+  };
 
   return (
     <>
-    <div>
-      <h1 className='text-blue-500 underline text-center'>Hello MAH</h1>
-    </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>
+        <div className="flex gap-5">
+          <button type="button" onClick={() => setCount(count + 1)}>
+            Increase
+          </button>
+          <button type="button" onClick={decreaseNum}>
+            Decrease
+          </button>
+        </div>
+        <div className="text-3xl p-4 bg-gray-700 m-4 rounded-xl ">{count}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
